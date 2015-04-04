5c111ac6011603966ddf377a51a6c9a5848d1ade
YanshuH <hys235606751@gmail.com>
2015-04-05 00:44:42 +0200

    add overlay and game start/restart control

--- js/index.js
+++ js/index.js
@@ -5,9 +5,15 @@ var BoardView = React.createClass({
   getInitialState: function () {
     return {board: this.props.board};
   },
+
+  startGame: function () {
+    this.setState(this.getInitialState());
+  },
+
   handleCellClickClick: function(cell) {
     // TODO
   },
+
   render: function () {
     var self = this;
     var cells = this.state.board.cells.map(function (row) {
@@ -23,6 +29,7 @@ var BoardView = React.createClass({
     return (
       <div className='board'>
         {cells}
+        <Overlay board={this.state.board} onStart={this.startGame} />
       </div>
     );
   }
@@ -33,6 +40,7 @@ var CellView = React.createClass({
   handleClick: function() {
       this.props.onCellClick(this);
   },
+
   render: function () {
     var cs = React.addons.classSet;
     var classes = cs({
@@ -42,6 +50,24 @@ var CellView = React.createClass({
     return (
       <span className={classes} onClick={this.handleClick}>{''}</span>
     );
+  }
+});
+
+
+var Overlay = React.createClass({
+  render: function () {
+    var board = this.props.board;
+
+    // TODO: is first start of already end
+    this.props.buttonWording = 'Start';
+    this.props.wording = 'GO GO GO';
+
+    return (
+      <div className='overlay'>
+        <p className='message'>{this.props.wording}</p>
+        <button className="startButton b-lblue" onClick={this.props.onStart}>{this.props.buttonWording}</button>
+      </div>
+    )
   }
 });
 
