578ab3c6f17f1857b43c8cdc11a8c83f0db272e0
YanshuH <hys235606751@gmail.com>
2015-04-05 17:29:14 +0200

    pinripal game control, start and end points

--- js/index.js
+++ js/index.js
@@ -1,17 +1,32 @@
 /** @jsx React.DOM */
-var board = new Board(3, 500, 400);
-
 var BoardView = React.createClass({
   getInitialState: function () {
-    return {board: this.props.board};
+    return {
+      board: new Board(3, 1000, 2000),
+      showOverlay: true
+    };
   },
 
-  startGame: function () {
-    this.setState(this.getInitialState());
+  startGame: function (overlay) {
+    this.setState({
+      board: this.state.board,
+      showOverlay: false
+    });
   },
 
-  handleCellClickClick: function(cell) {
-    // TODO
+  handleCellClickClick: function (cell) {
+    // TODO: statistics
+  },
+
+  handleCellGenerate: function (cell) {
+    // TODO: check if board's cells have all tiles
+    if (this.state.board.checkCells()) {
+      this.props.endGame = true;
+      this.setState(this.getInitialState());
+
+      // Stop cell interval
+      return true;
+    }
   },
 
   render: function () {
@@ -20,7 +35,11 @@ var BoardView = React.createClass({
       return (<div className='row'>
         {
           row.map(function (col) {
-            return <CellView tile={col} onCellClick={self.handleCellClickClick} />;
+            return <CellView
+              tile={col}
+              onCellClick={self.handleCellClickClick}
+              onCellGenerate={self.handleCellGenerate}
+              endGame={self.props.endGame} />;
           })
         }
       </div>);
@@ -29,7 +48,7 @@ var BoardView = React.createClass({
     return (
       <div className='board'>
         {cells}
-        <Overlay board={this.state.board} onStart={this.startGame} />
+        <Overlay board={this.state.board} show={this.state.showOverlay} onStart={this.startGame} />
       </div>
     );
   }
@@ -37,15 +56,45 @@ var BoardView = React.createClass({
 
 
 var CellView = React.createClass({
-  handleClick: function() {
+  getInitialState: function () {
+    return { tile: this.props.tile };
+  },
+
+  handleClick: function () {
+      // Board callback function for global event handle
       this.props.onCellClick(this);
+      this.state.tile.setHide();
+      this.setState({ tile: this.state.tile });
+  },
+
+  // Core function for tiles generation and callbacks to board
+  componentWillReceiveProps: function (oldProps, newProps) {
+    if (oldProps.endGame === true) {
+      return;
+    }
+    var self = this;
+    // Generate tiles by input interval of board
+    self.props.intervalVariable = setInterval(function () {
+      // Random generation
+      if (oldProps.tile.getRandomBoolean()) {
+        // show tile and update the rendering
+        oldProps.tile.setShow();
+        self.setState({ tile: oldProps.tile });
+
+        // Callback function in board, check if the game is over
+        if (self.props.onCellGenerate(oldProps.tile)) {
+          clearInterval(self.props.intervalVariable);
+          console.log('game ends');
+        }
+      }
+    }, oldProps.tile.interval);
   },
 
   render: function () {
     var cs = React.addons.classSet;
     var classes = cs({
       'cell': true,
-      'tile b-red': this.props.tile !== null ? true : false
+      'tile b-red': this.state.tile.hasShown() ? true : false
     });
     return (
       <span className={classes} onClick={this.handleClick}>{''}</span>
@@ -55,20 +104,29 @@ var CellView = React.createClass({
 
 
 var Overlay = React.createClass({
+  handleClick: function() {
+    this.setState({ show: false });
+    this.props.onStart(this);
+  },
   render: function () {
+    var cs = React.addons.classSet;
     var board = this.props.board;
-
     // TODO: is first start of already end
     this.props.buttonWording = 'Start';
     this.props.wording = 'GO GO GO';
 
+    var classes = cs({
+      'overlay': true,
+      'visible': this.props.show,
+      'hidden': !this.props.show
+    });
     return (
-      <div className='overlay'>
+      <div className={classes}>
         <p className='message'>{this.props.wording}</p>
-        <button className="startButton b-lblue" onClick={this.props.onStart}>{this.props.buttonWording}</button>
+        <button className="startButton b-lblue" onClick={this.handleClick}>{this.props.buttonWording}</button>
       </div>
     )
   }
 });
 
-React.render(<BoardView board={board} />, document.getElementById('board'));
+React.render(<BoardView />, document.getElementById('board'));
